481c6478f9a520ff00ba8bbea027d2b4f5ed262a
Tyler Hillsman <tyler.hillsman@gmail.com>
2023-04-05 14:37:07 -0500

    Initial Commit

---
+++ Knapsack/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Knapsack
+//
+//  Created by Tyler Hillsman on 4/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Knapsack/KnapsackApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnapsackApp.swift
+//  Knapsack
+//
+//  Created by Tyler Hillsman on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct KnapsackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
